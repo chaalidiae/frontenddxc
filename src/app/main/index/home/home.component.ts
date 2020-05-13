3c011f45eb10ac11fa3b0f1d868deebf1c38df4d
unknown <med.benali.pro@gmail.com>
2020-05-13 16:01:48 +0200

    fix language and style

--- src/app/main/index/home/home.component.ts
+++ src/app/main/index/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import * as fromI18n from '../../../shared/lang/i18n/reducers';
+import { I18nComponent } from 'src/app/shared/lang/i18n/container/i18n.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends I18nComponent {
 
-  constructor() { }
+  constructor(
+    readonly store: Store<fromI18n.State>,
+    readonly translate: TranslateService) {
+      super(store, translate);
+     }
 
-  ngOnInit(): void {
-  }
+ 
 
 }
--- src/app/main/index/home/home.module.ts
+++ src/app/main/index/home/home.module.ts
@@ -3,13 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { I18nModule } from 'src/app/shared/lang/i18n/i18n.module';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../../../../assets/i18n/', '/home.json');
+}
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    RouterModule,
+    I18nModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      },
+      isolate: true
+    })
   ]
 })
 export class HomeModule { }
